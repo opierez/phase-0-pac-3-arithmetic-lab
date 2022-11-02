681b34f6d7c36ab64f32e80142650d6aeccffc53
olivia <opierez@gmail.com>
2022-11-02 18:34:12 -0400

    commit tree a7b0799eea7c9bfcc54dd4d766ca37976cafb523

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(num) {
+    return num += 1;
+}
+
+function decrement(num) {
+    return num -= 1;
+}
+
+function makeInt(num) {
+    return parseInt(num, 10);
+}
+
+function preserveDecimal(num) {
+    return parseFloat(num);
+}
